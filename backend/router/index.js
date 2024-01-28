4a16bb100ba265c932a5b0c6652191b5704be5c2
viper <flutterydev@gmail.com>
2024-01-28 10:56:22 +0530

    task 4 done

--- backend/router/index.js
+++ backend/router/index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const { verifyUser, createUser } = require("./types");
 const { userModel } = require("./db");
 const { signJwt, verifyJwt } = require("./jwt");
+const userRouter = require("./user");
 
 const router = express.Router();
 
 router.use(express.json());
+
+router.use("/user", userRouter);
 
 router.get("/", (req, res) => {
     res.json({
