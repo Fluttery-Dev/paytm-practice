019fd6860cd24a7b4461ebabbcb50471086a6f99
MoonstarDev <admin.development@gameduels.in>
2024-02-02 15:48:35 +0530

    Fixed the dashboard glitch and the circular json error

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const rootRouter = require("./router/index");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1", rootRouter);
 
-
-app.listen(3000);
+app.listen(3000, () => console.log(`Server started at port ${3000}`));
